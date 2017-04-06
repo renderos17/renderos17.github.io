5966cb8c842ad8151c34620ea0a8d6ff6d4f2045
Alexis Renderos Dev Laptop <renderosalexis17@gmail.com>
2017-04-06 15:04:09 -0500

    It worked, but I'm going to tinker a bit more. -Alexis

--- js/Kurama.js
+++ js/Kurama.js
@@ -4,67 +4,38 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 }, false);
 
-/**
- * Creates a new BABYLON Engine and initialize the scene
- */
 function initScene() {
-// Get the canvas element from our HTML above
-var canvas = document.getElementById("renderCanvas");
 
-// Load the BABYLON 3D engine
-var engine = new BABYLON.Engine(canvas, true);
+	var canvas = document.getElementById("renderCanvas");
+	var engine = new BABYLON.Engine(canvas, true);
 
-// This begins the creation of a function that we will 'call' just after it's built
 var createScene = function () {
-	// Now create a basic Babylon Scene object 
 	var scene = new BABYLON.Scene(engine);
-
-	// Change the scene background color to green.
-	scene.clearColor = new BABYLON.Color3(0, 1, 0);
+	scene.clearColor = new BABYLON.Color3(0, 1, 0); // Bright green.
 
 	var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
 
-	// This creates a light, aiming 0,1,0 - to the sky.
 	var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-	// Dim the light a small amount
 	light.intensity = .5;
 
-	// var light2 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(0, 10, 100), scene);
-
-	// Let's try our built-in 'sphere' shape. Params: name, subdivisions, size, scene
 	var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 4, scene);
-
 	var materialSphere1 = new BABYLON.StandardMaterial("texture1", scene);
-
-	// materialSphere1.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
-
-	materialSphere1.diffuseTexture = new BABYLON.Texture("textures/flower1.jpg", scene);
-
+	materialSphere1.diffuseTexture = new BABYLON.Texture("textures/pcb.svg", scene);
 	sphere.material = materialSphere1;
-
-	// Move the sphere upward 1/2 its height
 	sphere.position.y = 2;
 
-    var plane = BABYLON.Mesh.CreatePlane("plane", 120, scene);
+    var plane = BABYLON.Mesh.CreatePlane("plane", 100, scene);
     plane.position.y = -5;
     plane.rotation.x = Math.PI / 2;
-
     var materialPlane = new BABYLON.StandardMaterial("texturePlane", scene);
-    materialPlane.diffuseTexture = new BABYLON.Texture("textures/space.jpg", scene);
-    materialPlane.diffuseTexture.uScale = 5.0;//Repeat 5 times on the Vertical Axes
-    materialPlane.diffuseTexture.vScale = 5.0;//Repeat 5 times on the Horizontal Axes
+    materialPlane.diffuseTexture = new BABYLON.Texture("textures/space1.jpg", scene);
+    materialPlane.diffuseTexture.uScale = 10.0;
+    materialPlane.diffuseTexture.vScale = 10.0;
     materialPlane.backFaceCulling = false;
-
     plane.material = materialPlane;
 
-    // This targets the camera to the sphere
-	camera.setTarget(sphere);
+	camera.attachControl(canvas, false); // req
 
-	// This attaches the camera to the canvas
-	camera.attachControl(canvas, false);
-
-	// Leave this function
 	return scene;
 };
 
