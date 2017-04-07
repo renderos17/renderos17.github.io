58881036d584fee5a8efe20c0f8016cd3176ffef
Alexis Renderos Dev Laptop <renderosalexis17@gmail.com>
2017-04-07 14:54:46 -0500

    rip -Alexis

--- js/Kurama.js
+++ js/Kurama.js
@@ -39,10 +39,11 @@ var createScene = function () {
     plane1.rotation.x = Math.PI / 2;
         
     var plane2 = BABYLON.Mesh.CreatePlane("planeNor", 100, scene);
-    plane2.position.x = 50;
-    plane2.position.y = -50;
-    plane2.position.z = 0;
+    plane2.position.x = 0;
+    plane2.position.y = 50;
+    plane2.position.z = 50;
     plane2.rotation.x = Math.PI / 2;
+    plane2.rotation.y = Math.PI / 2;
     
     var plane3 = BABYLON.Mesh.CreatePlane("planeEas", 100, scene); // Good!
     plane3.position.x = 50;
@@ -55,6 +56,7 @@ var createScene = function () {
     plane4.position.y = 50;
     plane4.position.z = 0;
     plane4.rotation.x = Math.PI / 2;
+    plane4.rotation.y = Math.PI / 2;
 
     var plane5 = BABYLON.Mesh.CreatePlane("planeWes", 100, scene); // Good!
     plane5.position.x = -50;
