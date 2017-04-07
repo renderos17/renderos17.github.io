3814d04bd207ff130d2cad1296ab4acc648e895d
Alexis Renderos Dev Laptop <renderosalexis17@gmail.com>
2017-04-07 10:12:35 -0500

    Still playing with the boxes. -Alexis

--- js/Kurama.js
+++ js/Kurama.js
@@ -26,27 +26,33 @@ var createScene = function () {
 	sphere.material = materialSphere1;
 	sphere.position.y = 2;
 
-    var plane1 = BABYLON.Mesh.CreatePlane("planeBot", 100, scene);
+    var plane1 = BABYLON.Mesh.CreatePlane("planeBot", 100, scene); // Good!
     plane1.position.y = 0;
     plane1.rotation.x = Math.PI / 2;
         
     var plane2 = BABYLON.Mesh.CreatePlane("planeNor", 100, scene);
-    plane2.position.y = 100;
+    plane2.position.x = -50;
+    plane2.position.y = -50;
+    plane2.position.z = -50;
     plane2.rotation.y = Math.PI / 2;
     
     var plane3 = BABYLON.Mesh.CreatePlane("planeEas", 100, scene);
-    plane3.position.y = -100;
+    plane3.position.x = 50
+    plane3.position.y = 50;
     plane3.rotation.y = 0;
 
     var plane4 = BABYLON.Mesh.CreatePlane("planeSou", 100, scene);
-    plane4.position.x = 100;
+    plane4.position.x = 50;
+    plane4.position.y = 50;
+    plane4.position.z = 50;
     plane4.rotation.y = Math.PI / 2;
 
     var plane5 = BABYLON.Mesh.CreatePlane("planeWes", 100, scene);
-    plane5.position.x = -100;
-    plane5.rotation.y = 0;
+    plane5.position.x = -50;
+    plane5.position.y = 50;
+    plane5.rotation.y = Math.PI / 2;
     
-    var plane6 = BABYLON.Mesh.CreatePlane("planeTop", 100, scene);
+    var plane6 = BABYLON.Mesh.CreatePlane("planeTop", 100, scene); // Good!
     plane6.position.y = 100;
     plane6.rotation.x = Math.PI / 2;
 
@@ -65,8 +71,8 @@ var createScene = function () {
     plane1.material = materialPlane;
     plane2.material = materialPlane;
     plane3.material = materialPlane;
-    plane4.material = materialPlane;
-    plane5.material = testingPlane;
+    plane4.material = testingPlane;
+    plane5.material = materialPlane;
     plane6.material = materialPlane;
 
 	camera.attachControl(canvas, false); // req
