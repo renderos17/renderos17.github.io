567f32b712a7ca03154359d379d1c0ca3d4b939f
Alexis Renderos Dev Laptop <renderosalexis17@gmail.com>
2017-04-10 13:27:47 -0500

    YEAH BOY IT WORKED! LETS TRY RESIZING THIS AND PROBABLY BREAK IT -Alexis

--- js/Kurama.js
+++ js/Kurama.js
@@ -26,6 +26,8 @@ var createScene = function () {
 	sphere.material = materialSphere1;
 	sphere.position.y = 2;
 
+	var boxSize = 500;
+
 	// Placement rules for planes:
 	//
 	// You shall not use a negative Y value.
@@ -34,38 +36,34 @@ var createScene = function () {
 	// ...
 	// That is all.
 
-    var plane1 = BABYLON.Mesh.CreatePlane("planeBot", 100, scene); // Good!
+    var plane1 = BABYLON.Mesh.CreatePlane("planeBot", boxSize, scene); // Good!
     plane1.position.y = 0;
     plane1.rotation.x = Math.PI / 2;
         
-    var plane2 = BABYLON.Mesh.CreatePlane("planeNor", 100, scene);
+    var plane2 = BABYLON.Mesh.CreatePlane("planeNor", boxSize, scene);
     plane2.position.x = 0;
-    plane2.position.y = 50;
-    plane2.position.z = 50;
-    // plane2.rotation.x = Math.PI / 2;
-    // plane2.rotation.y = Math.PI / 2;
+    plane2.position.y = boxSize / 2.0;
+    plane2.position.z = boxSize / 2.0;
     
-    var plane3 = BABYLON.Mesh.CreatePlane("planeEas", 100, scene); // Good!
-    plane3.position.x = 50;
-    plane3.position.y = 50;
+    var plane3 = BABYLON.Mesh.CreatePlane("planeEas", boxSize, scene); // Good!
+    plane3.position.x = boxSize / 2.0;
+    plane3.position.y = boxSize / 2.0;
     plane3.position.z = 0;
     plane3.rotation.y = Math.PI / 2;
 
-    var plane4 = BABYLON.Mesh.CreatePlane("planeSou", 100, scene);
+    var plane4 = BABYLON.Mesh.CreatePlane("planeSou", boxSize, scene);
     plane4.position.x = 0;
-    plane4.position.y = 50;
-    plane4.position.z = -50;
-    // plane4.rotation.x = Math.PI / 2;
-    // plane4.rotation.y = Math.PI / 2;
+    plane4.position.y = boxSize / 2.0;
+    plane4.position.z = -(boxSize / 2.0);
 
-    var plane5 = BABYLON.Mesh.CreatePlane("planeWes", 100, scene); // Good!
-    plane5.position.x = -50;
-    plane5.position.y = 50;
+    var plane5 = BABYLON.Mesh.CreatePlane("planeWes", boxSize, scene); // Good!
+    plane5.position.x = -(boxSize / 2.0);
+    plane5.position.y = boxSize / 2.0;
     plane5.position.z = 0;
     plane5.rotation.y = Math.PI / 2;
     
-    var plane6 = BABYLON.Mesh.CreatePlane("planeTop", 100, scene); // Good!
-    plane6.position.y = 100;
+    var plane6 = BABYLON.Mesh.CreatePlane("planeTop", boxSize, scene); // Good!
+    plane6.position.y = boxSize;
     plane6.rotation.x = Math.PI / 2;
 
     var materialPlane = new BABYLON.StandardMaterial("texturePlane", scene);
