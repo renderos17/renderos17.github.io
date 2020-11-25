f6df231ff5ed74e605c61a9c91d4cfb59cbbd724
Alexis Renderos <renderosalexis17@gmail.com>
2020-11-25 13:27:51 -0600

    Hotfix for edge cases in bookingsuccessful.js. -Alexis

--- js/calendlyintegration.js
+++ js/calendlyintegration.js
@@ -3,11 +3,13 @@ function convertUTCDateToLocalDate(date) {
     return newDate;
 }
 
-
-
 let params = new URLSearchParams(location.search);
 let appointmentName = params.get('invitee_full_name');
-appointmentName = appointmentName.substring(0, appointmentName.indexOf(" "));
-document.getElementById("name").innerHTML = appointmentName;
+if (appointmentName.indexOf(" ") > 0){
+  appointmentName = appointmentName.substring(0, appointmentName.indexOf(" "));
+}
+document.getElementById("name").innerHTML = ", " + appointmentName;
 let appointmentDate = new Date(Date.parse(params.get('event_start_time')));
 document.getElementById("date").innerHTML = convertUTCDateToLocalDate(appointmentDate);
+let appointmentEmail = params.get('invitee_email');
+document.getElementById("email").innerHTML = " sent to " + appointmentEmail;
